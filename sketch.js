df8210c3f34111a7f52b6bed0ef9f1e8fd61f7ba
Gustav Paragas <gep21b@fsu.edu>
2022-10-18 12:42:55 -0400

    project update

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
 let w = 500;
 let h = 600;
 
-
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(500,400);
 }
 
-function draw() {
-  background(125);
+function draw(){
+  background (125);
+  fill("red");
   translate(50, 50);
-  fill(red)
-stroke(355, 0, 0);
+stroke(300, 0, 0);
 beginShape();
 // Exterior part of shape, clockwise winding
 vertex(-40, -40);
