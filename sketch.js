88079d69d879ed8f5dba4c60accd3c4ac4eabee1
Gustav Paragas <gep21b@fsu.edu>
2022-10-18 12:27:39 -0400

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let w = 500;
+let h = 600;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
 }
 
 function draw() {
   background(125);
+  translate(50, 50);
+  fill(red)
+stroke(355, 0, 0);
+beginShape();
+// Exterior part of shape, clockwise winding
+vertex(-40, -40);
+vertex(40, -40);
+vertex(40, 40);
+vertex(-40, 40);
+// Interior part of shape, counter-clockwise winding
+beginContour();
+vertex(-20, -20);
+vertex(-20, 20);
+vertex(20, 20);
+vertex(20, -20);
+endContour();
+endShape(CLOSE);
 }
